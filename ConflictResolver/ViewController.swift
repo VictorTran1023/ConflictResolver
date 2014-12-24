f2261cc74c31745bc92c6be5fce8b0cd864c35fe
yiptran <Victor_Tran@Trans-Mac.local>
2014-12-24 22:19:08 +0800

    Initial Commit

---
+++ ConflictResolver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConflictResolver
+//
+//  Created by yiptran on 14/12/24.
+//  Copyright (c) 2014年 yiptran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
